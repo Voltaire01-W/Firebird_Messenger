2ab7e44c0404d6b3fcf093ca2fe256a5c3643611
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-05-29 22:30:23 -0500

    Added Chats component with ChatEngine.io, will need to create environment variables for secrets.

---
+++ src/components/Chats.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
+import { auth } from '../firebase';
+
+const Chats = () => {
+    const history = useHistory();
+
+    const handleLogout = async () => {
+        await auth.signOut();
+        
+        history.push('/');
+    }
+
+    return (
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    Firebird Messenger
+                </div>
+                <div onClick={handleLogout} className="logout-tab">
+                    Logout
+                </div>
+            </div>
+
+            <ChatEngine 
+                height="calc(100vh - 66px)"
+                projectId="ID"
+                userName="."
+                userSecret="."
+            />
+        </div>
+    )
+}
+
+export default Chats
